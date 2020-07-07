e23509d591ba5792031a153eb67bcb88c445dda0
Jon <orejajonathan@gmail.com>
2020-07-07 16:43:44 +0200

    user connection

---
+++ src/Factories.js
@@ -0,0 +1,46 @@
+const { uuid } = require('uuidv4');
+
+const createUser = ({name=""} = {})=>(
+
+    {
+        id:uuid(), 
+        name
+    }
+)
+
+
+
+const createMessage = ({message="", sender=""}= {} ) => (
+    {
+        id:uuid(),
+        time: getTime(new Date(Date.now())),
+        message,
+        sender
+    }
+)
+
+
+
+const createChat = ({messages=[], name="Community", users=[]}= {})=>(
+{
+    id:uuid(),
+    name,
+    messages,
+    users,
+    typingUsers:[],
+
+}
+)
+
+const getTime = (date) => {
+    return `${date.getHours()}:${("0"+date.getMinute()).slice(-2)}`
+}
+
+module.exports ={
+    createMessage,
+    createChat,
+    createUser
+}
+
+
+ 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import io from 'socket.io-client'
 import {USER_CONNECTED, LOGOUT} from '../Events'
-
+import LoginForm from './LoginForm'
 const socketUrl = "http://192.168.1.14:3231"
 
 export class Layout extends Component {
@@ -31,7 +31,7 @@ export class Layout extends Component {
 
 setUser = (user) =>{
     const {socket} = this.state
-    socket.emit(USER_CONNECTED);
+    socket.emit(USER_CONNECTED, user);
     this.setState({user})
 }
 
@@ -43,17 +43,16 @@ logout = () =>{
 
 
 
-
-
-
-
+  
     render() {
         const { title } = this.props
+        const { socket } = this.state
         return (
             <div className="container">
                 {title}
+                <LoginForm socket={socket} setUser={this.setUser}/>
             </div>
-        )
+        ) 
     }
 }
 
