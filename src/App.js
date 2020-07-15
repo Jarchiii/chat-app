2cd9b59cf931dd9bb45dcd8b14137262b5a6162a
Jon <orejajonathan@gmail.com>
2020-07-15 12:38:50 +0200

    new style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from './components/Layout'
-import './index.css'
+import './index.scss'
 function App() {
   return (
       <Layout title="Chat App BAby"/>
