838dc4f38b261b23bda8e6b47e6571facbc97786
Jon <orejajonathan@gmail.com>
2020-07-20 11:17:57 +0200

    express removed

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,9 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
+const app = require('http').createServer()
 const io = module.exports.io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3231
 
 const SocketManager = require('./SocketManager')
-app.use(express.static(__dirname + '/../../build'))
 io.on('connection' , SocketManager)
 
 server.listen(PORT, () => {
