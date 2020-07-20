68e0a8fbd6dd2f51a958d37a1005bc98cf126d69
Jon <orejajonathan@gmail.com>
2020-07-20 12:08:23 +0200

    deploy

---
+++ build/precache-manifest.7431c3bc27e661815839598cdd1ac917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2429fc6c8a384abdd777457ca9c50bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "821e6f675160a1f164eb",
+    "url": "/static/css/main.18931536.chunk.css"
+  },
+  {
+    "revision": "9a23fccc83aa2798499e",
+    "url": "/static/js/2.d21b23ec.chunk.js"
+  },
+  {
+    "revision": "9776b30d41b546d9529276da9650fb84",
+    "url": "/static/js/2.d21b23ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821e6f675160a1f164eb",
+    "url": "/static/js/main.9ecce5ad.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  }
+]);
