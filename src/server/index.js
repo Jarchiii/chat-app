16f0ab4f6d95ecc5d6edffa8ec4bf0acb99b7609
Jon <orejajonathan@gmail.com>
2020-07-20 11:29:00 +0200

    a

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,21 @@
 const app = require('http').createServer()
 const io = module.exports.io = require('socket.io')(server)
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3231
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials : true,
+  
+  
+    /* credentials : Configures the Access-Control-Allow-Credentials CORS header. Set to true to pass the header, otherwise it is omitted  https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Credentials */
+    optionsSuccessStatus: 200
+  };
+  
+  // cors middle on
+  app.use(cors(corsOptions));
+
 
 const SocketManager = require('./SocketManager')
 io.on('connection' , SocketManager)
